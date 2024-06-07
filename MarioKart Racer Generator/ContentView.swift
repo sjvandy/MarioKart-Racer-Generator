ecfe6bd8f2ad4fa8e194fbb1657d4545339c012a
Steven Vandegrift <svandegrift@icloud.com>
2024-06-06 23:57:33 -0700

    Create Base App
    App created prior to git

--- MarioKart Racer Generator/ContentView.swift
+++ MarioKart Racer Generator/ContentView.swift
@@ -7,15 +7,59 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View {        
+    @State private var currentCharacter = characters.randomElement()
+    @State private var currentKart = karts.randomElement()
+    @State private var currentWheels = wheels.randomElement()
+    @State private var currentGlider = gliders.randomElement()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text("MarioKart Racer Generator")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.white)
+                .padding()
+            Spacer()
+            Image(currentCharacter ?? "mario")
+                .padding()
+            Image(currentKart ?? "standardkart")
+                .padding()
+            HStack {
+                Image(currentWheels ?? "standardwheels")
+                    .padding()
+                Image(currentGlider ?? "superglider")
+                    .padding()
+            }
+            Spacer()
+            Button {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    currentCharacter = characters.randomElement()
+                    currentKart = karts.randomElement()
+                    currentWheels = wheels.randomElement()
+                    currentGlider = gliders.randomElement()
+                }
+            } label: {
+                Text("Generate")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(lineWidth: 5)
+                            .foregroundStyle(.white)
+                    )
+            }
+            Spacer()
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            Image("AppBackground")
+        }
     }
 }
 
