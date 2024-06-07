635d207fdb71a43795d77d48fdd3d8e573bebb33
Steven Vandegrift <svandegrift@icloud.com>
2024-06-06 22:57:28 -0700

    Initial Commit

---
+++ MarioKart Racer Generator/MarioKart_Racer_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioKart_Racer_GeneratorApp.swift
+//  MarioKart Racer Generator
+//
+//  Created by Steven Vandegrift on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MarioKart_Racer_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
